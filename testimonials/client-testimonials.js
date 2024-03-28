e8171ad7a56b5eb3707fc1762442018c57c17816
Benjamin Nneji <nnejiben@gmail.com>
2024-03-28 09:26:31 +0100

    Create client-testimonials.js
    Create the file where client testimonials will be stored.

---
+++ testimonials/client-testimonials.js
@@ -0,0 +1,39 @@
+// Content for Serome website client testimonials
+
+let testimonials = [
+  
+    // Client testimonial 01
+    {
+        comment: "Client commment goes here 01",
+        name: "Client Name 01",
+        location: "Client Location 01"
+    },
+  
+    // Client testimonial 02
+    {
+        comment: "Client commment goes here 02",
+        name: "Client Name 02",
+        location: "Client Location 02"
+    },
+  
+    // Client testimonial 03
+    {
+        comment: "Client commment goes here 03",
+        name: "Client Name 03",
+        location: "Client Location 03"
+    },
+  
+    // Client testimonial 04
+    {
+        comment: "Client commment goes here 04",
+        name: "Client Name 04",
+        location: "Client Location 04"
+    },
+  
+    // Client testimonial 05
+    {
+        comment: "Client commment goes here 05",
+        name: "Client Name 05",
+        location: "Client Location 05"
+    }
+];
