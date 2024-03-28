7fb3709177448d142bb0aab3e4b19fc516ccee5a
Benjamin Nneji <nnejiben@gmail.com>
2024-03-28 09:30:31 +0100

    Update client-testimonials.js

--- testimonials/client-testimonials.js
+++ testimonials/client-testimonials.js
@@ -4,35 +4,35 @@ let testimonials = [
   
     // Client testimonial 01
     {
-        comment: "Client commment goes here 01",
+        comment: "Client comment goes here 01",
         name: "Client Name 01",
         location: "Client Location 01"
     },
   
     // Client testimonial 02
     {
-        comment: "Client commment goes here 02",
+        comment: "Client comment goes here 02",
         name: "Client Name 02",
         location: "Client Location 02"
     },
   
     // Client testimonial 03
     {
-        comment: "Client commment goes here 03",
+        comment: "Client comment goes here 03",
         name: "Client Name 03",
         location: "Client Location 03"
     },
   
     // Client testimonial 04
     {
-        comment: "Client commment goes here 04",
+        comment: "Client comment goes here 04",
         name: "Client Name 04",
         location: "Client Location 04"
     },
   
     // Client testimonial 05
     {
-        comment: "Client commment goes here 05",
+        comment: "Client comment goes here 05",
         name: "Client Name 05",
         location: "Client Location 05"
     }
